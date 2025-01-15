5eb8155b5bde1c62c588221ce6ce4cd2fe861c99
Jan Mottl <mottja@delta-skola.cz>
2025-01-15 12:25:39 +0100

    kk

---
+++ 2B_S2/CSS_Rozšíření/Tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        super: "#f0f",
+      },
+      fontFamily: {
+        comic: "Comic Sans MS",
+      },
+    },
+  },
+  plugins: [],
+}
+
