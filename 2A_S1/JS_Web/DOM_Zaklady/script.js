2c35daaf4cabe7ce822b59aa9df1b61ad8cf3ecd
Jan Mottl <mottja@delta-skola.cz>
2025-03-12 14:46:48 +0100

    kk

--- 2A_S1/JS_Web/DOM_Zaklady/script.js
+++ 2A_S1/JS_Web/DOM_Zaklady/script.js
@@ -24,3 +24,32 @@ let ovoce2 = document.querySelectorAll('#list-section ul li');
 console.log(ovoce2);
 
 document.body.innerHTML += "Nic";
+
+//přidávání prvků
+
+let odstavec = document.createElement('p');
+odstavec.innerHTML = "Nějaký text";
+odstavec.style.color = "blue";
+
+document.body.appendChild(odstavec);
+
+//My Favorite Fruits
+
+let seznam = document.querySelector('#list-section ul');
+let noveOvoce;
+for (let index = 0; index < 20; index++) {
+    noveOvoce = document.createElement('li');
+    let text = document.createTextNode('Ananas');
+    noveOvoce.appendChild(text);
+    noveOvoce.className = "fruit-item";
+    noveOvoce.style.color = "yellow";
+    seznam.appendChild(noveOvoce);
+}
+
+let extraOvoce = document.createElement('li');
+extraOvoce.innerHTML = "Peach";
+
+
+seznam.replaceChild(extraOvoce,noveOvoce);
+
+seznam.removeChild(extraOvoce);
