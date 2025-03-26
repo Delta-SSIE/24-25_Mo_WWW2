795562d01fa2de5e1e9330543f46f7c43971a2af
Jan Mottl <mottja@delta-skola.cz>
2025-03-26 12:55:24 +0100

    kk

--- 2B_S2/JS_Web/JS-Paintbrush/script.js
+++ 2B_S2/JS_Web/JS-Paintbrush/script.js
@@ -5,16 +5,23 @@ let canvas = document.getElementById("paintRaster");
 let velikost = document.getElementById("velikost");
 let btn = document.getElementById("btn");
 
+let btnS = document.getElementById('stetec');
+let inputS = document.getElementById('velikoststetec');
+
+btnS.addEventListener("click",Vykresli);
 btn.addEventListener("click",Vykresli);
 
 function Vykresli()
 {
+    canvas.innerHTML = "";
     for (let index = 0; index < velikost.value; index++)
          {
             let radek = document.createElement("tr");
 
         for (let i = 0; i < velikost.value; i++) {
             let bunka = document.createElement("td");
+            bunka.style.width = inputS.value + "px";
+            bunka.style.height = inputS.value + "px";
             bunka.addEventListener("click",() => bunka.style.backgroundColor = barva.value)
             bunka.addEventListener("dragenter",() => bunka.style.backgroundColor = barva.value)
             radek.appendChild(bunka);            
